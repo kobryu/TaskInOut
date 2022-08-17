03f8257fc67ae2997df8b6b936265e8a9c710909
kobryu <ryusei.kobayashi0228@gmail.com>
2022-08-17 06:23:31 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,16 +16,41 @@
 #   )
 # end
 
-# 50.times do |n|
-# Task.create!(
-# user_id:2,
-# genre_id:2,
-# title: Faker::Lorem.sentence,
-# body: Faker::Lorem.sentences,
-# due_date: Faker::Date.between(from: '2022-07-26', to: '2022-08-10'),
-# )
+10.times do |n|
+Task.create!(
+user_id:2,
+genre_id:2,
+title: Faker::Lorem.sentence,
+body: Faker::Lorem.sentences,
+due_date: Faker::Date.between(from: '2022-08-18', to: '2022-08-30'),
+)
+end
 
+10.times do |n|
+Task.create!(
+user_id:4,
+genre_id:3,
+title: Faker::Lorem.sentence,
+body: Faker::Lorem.sentences,
+due_date: Faker::Date.between(from: '2022-08-18', to: '2022-08-30'),
+)
+end
+10.times do |n|
+Task.create!(
+user_id:2,
+genre_id:4,
+title: Faker::Lorem.sentence,
+body: Faker::Lorem.sentences,
+due_date: Faker::Date.between(from: '2022-08-18', to: '2022-08-30'),
+)
+end
 
-
-
+20.times do |n|
+Task.create!(
+user_id:5,
+genre_id:2,
+title: Faker::Lorem.sentence,
+body: Faker::Lorem.sentences,
+due_date: Faker::Date.between(from: '2022-08-18', to: '2022-08-30'),
+)
 end
